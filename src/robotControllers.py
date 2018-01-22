f4c75baf7bee78727918732f9b13249596647732
Juanjoglvz <JuanJose.Corroto@alu.uclm.es>
2018-01-22 13:40:18 +0100

    defenders should move away from allies now

--- src/robotControllers.py
+++ src/robotControllers.py
@@ -16,6 +16,12 @@ class RobotControllerDefI(drobotscomm.RobotControllerSlave):
         self.bot = bot
         self.name = name
         self.location = None
+        self.damage = 0
+        self.ourobotslocation = None
+        self.energy = 100
+        self.moveCounter = -1
+        self.direction = -500
+        self.moving = False
         print("Created RobotController {}, for bot {}".format(name, repr(bot)))
         sys.stdout.flush()
 
@@ -23,14 +29,88 @@ class RobotControllerDefI(drobotscomm.RobotControllerSlave):
         return self.location
 
     def turn(self, current):
-        self.location = self.bot.location()
         print("Turn of {} at location {},{}".format(
             id(self), self.location.x, self.location.y))
         sys.stdout.flush()
 
+        self.getActualLocation()
+
+        if self.moveCounter > 0:
+            self.moveCounter -= 1
+
+        if self.moveCounter == 0:
+            self.move(0, 0)
+            self.moveCounter = -1
+
+        ourobotslocation = self.robotcontainer.list()
+        ourobotslocation = list(map(lambda x: x.getLocation(), ourobotslocation.values()))
+
+        for ourRobotPos in ourobotslocation:
+            if ourRobotPos.x != self.location.x and ourRobotPos.y != self.location.y:
+                if self.shouldMoveAway(ourRobotPos):
+                    if self.canmove(100):
+                        self.moveAway(ourRobotPos)
+
+        self.energy = 100
+
     def robotDestroyed(self, current):
         print("Robot destroyed, robot position: {},{}".format(self.location.x, self.location.y))
         sys.stdout.flush()
+
+    def getActualLocation(self):
+        print("Bot {} asked for its location".format(self.bot))
+        sys.stdout.flush()
+        self.location = self.bot.location()
+        self.energy -= 1
+
+    def getActualDamage(self):
+        self.damage = self.bot.damage()
+        self.energy -= 1
+
+    def shouldMoveAway(self, otherPosition):
+        return self.calculateDistance(otherPosition) < 80 and self.moveCounter <= 0
+
+    def calculateDistance(self, point):
+        distance = int(math.sqrt(math.pow((self.location.x - point.x), 2) +
+                                 math.pow((self.location.y - point.y), 2)))
+
+        print("Calculated distance {}".format(distance))
+        sys.stdout.flush()
+
+        return distance
+#       return euclidean distance to point
+
+    def calculateAngle(self, point):
+        vector = [point.x - self.location.x, point.y - self.location.y]
+
+        angle = math.atan2(vector[1], vector[0]) * (180/math.pi)
+
+        if angle < 0:
+            angle += 360
+
+        print("Calculated angle {}".format(angle))
+        sys.stdout.flush()
+
+        return int(angle)
+
+    def canmove(self, speed):
+        return (self.energy >= 60 and speed != 0) or (self.energy >= 1 and speed == 0)
+
+    def moveAway(self, otherPosition):
+        angle = self.calculateAngle(otherPosition)
+        angle = (angle + 180) % 360  # calculate opposite angle
+        speed = 100
+        if self.canmove(speed):
+            print("Moving away from {} with angle {}".format(otherPosition, angle))
+            sys.stdout.flush()
+            self.move(angle, speed)
+            self.moveCounter = 10
+
+    def move(self, angle, speed):
+        self.bot.drive(angle, speed)
+        self.energy -= 1 if speed == 0 else 60
+        self.moving = speed != 0
+        self.direction = -500 if speed == 0 else angle
 
 
 class RobotControllerAttI(drobotscomm.RobotControllerSlave):
@@ -159,7 +239,7 @@ class RobotControllerAttI(drobotscomm.RobotControllerSlave):
             ourobotslocation = self.robotcontainer.end_list(ourobotspromise)
             ourobotslocation = list(map(lambda x: x.getLocation(), ourobotslocation.values()))
 
-        # Moving away from enemies logic
+        # Moving away from allies logic
         for ourRobotPos in ourobotslocation:
             if ourRobotPos.x != self.location.x and ourRobotPos.y != self.location.y:
                 if self.shouldMoveAway(ourRobotPos):
@@ -195,8 +275,6 @@ class RobotControllerAttI(drobotscomm.RobotControllerSlave):
 
     def iscompanion(self, ourRobotPos, robotPos):
         return self.checkPointInCircle(ourRobotPos, 6, robotPos)
-        return ourRobotPos.x -2 <= robotPos.x and ourRobotPos.x + 2 >= robotPos.x and \
-            ourRobotPos.y -2 <= robotPos.y and ourRobotPos.y + 2 >= robotPos.y
 
     def getActualLocation(self):
         print("Bot {} asked for its location".format(self.bot))
